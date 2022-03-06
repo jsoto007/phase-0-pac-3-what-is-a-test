8fbf4b65f7f7c05eb1b5cada0be2b925e25c8084
Jose Soto <jsoto7087@gmail.com>
2022-03-05 20:49:09 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,17 @@
 const { name, height, message } = require("../index.js");
 
-/*
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+    it('returns "Joe"', () => {
+      expect(name).toEqual('Joe')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+    it('is less than 69', () => {
+      expect(height).toBeLessThan(69)
     })
   })
 
@@ -22,4 +22,4 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
+
